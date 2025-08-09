752a894e99884f28b3305b9c6436e77b2330baf6
Abhishek Sharma <abhisheksharma06022006@gmail.com>
2025-08-09 00:45:59 +0000

    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsby

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2365487260750859"
+     crossOrigin="anonymous"></script>
       </head>
       <body className="font-body antialiased">
         {children}
