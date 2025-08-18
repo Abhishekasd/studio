3c7406bfd49dfbd9269ac917f45bc1ef04e5c5ca
Abhishek Sharma <abhisheksharma06022006@gmail.com>
2025-08-18 17:05:00 +0000

    remove the spline animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,11 @@ import { MorningMuseClient } from "@/components/morning-muse-client";
 
 export default function Home() {
   return (
-    <div className="relative z-10 flex min-h-screen flex-col items-center justify-center pt-24">
-      <div
-        style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          zIndex: -1,
-          pointerEvents: 'none',
-        }}
-      >
-        <iframe
-          src="https://my.spline.design/orb-bFuqF8lCEvWM4o0m7qVBfTzV/"
-          frameBorder="0"
-          width="100%"
-          height="100%"
-          style={{ pointerEvents: 'auto' }}
-        ></iframe>
-      </div>
-      <main className="flex-grow flex flex-col items-center justify-center p-4 sm:p-6 md:p-8 w-full">
+    <div className="flex min-h-screen flex-col items-center justify-center pt-24">
+      <main className="flex-grow flex flex-col items-center justify-center p-4 sm:p-6 md:p-8 w-full z-10">
         <MorningMuseClient />
       </main>
+      <div className="fixed inset-0 -z-10 h-full w-full bg-background" />
     </div>
   );
 }
