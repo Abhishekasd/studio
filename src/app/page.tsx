d75477c73bf5793812311a0c8e2fe83fb9fab896
Abhishek Sharma <abhisheksharma06022006@gmail.com>
2025-09-14 02:44:27 +0000

    still not visible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import MainPage from "../components/main-page";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center pt-24 bg-transparent">
+    <div className="flex min-h-screen flex-col items-center justify-center pt-24">
       <main className="flex-grow flex flex-col items-center justify-center p-4 sm:p-6 md:p-8 w-full">
         <MainPage />
       </main>
